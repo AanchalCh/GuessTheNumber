db96299a07455655a198916d62a59a45d3552d78
AanchalCh <aanchal.chaurasia99@gmail.com>
2022-02-27 00:17:54 +0530

    Refactoring code

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ let secretNumber = Math.trunc(Math.random()*20) +1
 let updateScore = 20
 let updateHighScore = 0
 
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message
+}
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = (Number)(document.querySelector('.guess').value)
 
     if(!guess){
-        document.querySelector('.message').textContent = "You forgot to enter a number"
+        displayMessage("You forgot to enter a number")
     }
     else if(guess==secretNumber){
-        document.querySelector('.message').textContent = "Correct Number!"
+        displayMessage("Correct Number!")
         document.querySelector('.number').textContent = secretNumber
         document.querySelector('body').style.backgroundColor = "#60b347"
 
@@ -20,37 +24,51 @@ document.querySelector('.check').addEventListener('click', function(){
         updateHighScore = Math.max(updateHighScore, updateScore)
         document.querySelector('.highscore').textContent = updateHighScore
     }
-    else if(guess>secretNumber){
+
+    else if(guess!=secretNumber){
         if(updateScore>1)
         {
-            document.querySelector('.message').textContent = "Too High"
+            displayMessage(guess < secretNumber ? "Too Low" : "Too High")
             updateScore--
             document.querySelector('.score').textContent = updateScore
         }
         else{
-            document.querySelector('.message').textContent = "You lost the game!"
-            document.querySelector('.score').textContent = 0
-        }
-    }
-    else if (guess<secretNumber){
-        if(updateScore>1)
-        {
-            document.querySelector('.message').textContent = "Too Low"
-            updateScore--
-            document.querySelector('.score').textContent = updateScore
-        }
-        else{
-            document.querySelector('.message').textContent = "You lost the game!"
+            displayMessage("You lost the game!")
             document.querySelector('.score').textContent = 0
         }
     }
 })
+//     else if(guess>secretNumber){
+//         if(updateScore>1)
+//         {
+//             document.querySelector('.message').textContent = "Too High"
+//             updateScore--
+//             document.querySelector('.score').textContent = updateScore
+//         }
+//         else{
+//             document.querySelector('.message').textContent = "You lost the game!"
+//             document.querySelector('.score').textContent = 0
+//         }
+//     }
+//     else if (guess<secretNumber){
+//         if(updateScore>1)
+//         {
+//             document.querySelector('.message').textContent = "Too Low"
+//             updateScore--
+//             document.querySelector('.score').textContent = updateScore
+//         }
+//         else{
+//             document.querySelector('.message').textContent = "You lost the game!"
+//             document.querySelector('.score').textContent = 0
+//         }
+//     }
+// })
 
 document.querySelector('.again').addEventListener('click', function () {
     updateScore = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
   
-    document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...')
     document.querySelector('.score').textContent = updateScore;
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = '';
