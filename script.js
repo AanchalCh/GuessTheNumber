931c05e0e16ccd4509c55a1a35245b2a6de1d04a
AanchalCh <aanchal.chaurasia99@gmail.com>
2022-02-26 21:57:16 +0530

    Updating scores and implementing game logic

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random()*20) +1
+document.querySelector('.number').textContent = secretNumber;
+let updateScore = 20
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = (Number)(document.querySelector('.guess').value)
 
     if(!guess){
-        document.querySelector('.message').textContent = "Ypu forgot to enter a number"
+        document.querySelector('.message').textContent = "You forgot to enter a number"
+    }
+    else if(guess==secretNumber){
+        document.querySelector('.message').textContent = "Correct Number!"
+    }
+    else if(guess>secretNumber){
+        document.querySelector('.message').textContent = "Too High"
+        updateScore--
+        document.querySelector('.score').textContent = updateScore
+    }
+    else{
+        document.querySelector('.message').textContent = "Too Low"
+        updateScore--
+        document.querySelector('.score').textContent = updateScore
     }
 }
 )
